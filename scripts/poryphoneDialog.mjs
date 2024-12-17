ef11e9a9275486610cb873cc7e0fc34848637e0f
Estelle <loutiko@gmail.com>
2024-12-17 10:59:26 +0100

    apps

--- scripts/poryphoneDialog.mjs
+++ scripts/poryphoneDialog.mjs
@@ -5,20 +5,43 @@ export default class PoryphoneDialog extends FormApplication {
     const defaults = super.defaultOptions;
 
     const overrides = {
-      height: "auto",
+      height: 500,
+      width: 320,
       id: Poryphone.ID + "-dialog",
       template: Poryphone.TEMPLATES.DIALOG,
       title: "PORYPHONE.TITLE",
       userId: game.userId,
     };
 
-    const mergedOptions = foundry.utils.mergeObject(defaults, overrides);
-
-    return mergedOptions;
+    return foundry.utils.mergeObject(defaults, overrides);
   }
   getData(options) {
+    const scannerApp = {
+      title: "Pokéscan",
+      icon: "binoculars",
+    };
+    const pokedexApp = {
+      title: "Pokédex",
+      icon: "address-book",
+    };
+    const teamApp = {
+      title: "Ma team",
+      icon: "users",
+    };
+    const meApp = {
+      title: "Mes stats",
+      icon: "circle-user",
+    };
+    const bagApp = {
+      title: "Mon sac",
+      icon: "bag-shopping",
+    };
+    const settingsApp = {
+      title: "Options",
+      icon: "cog",
+    };
     return {
-      truc: "Bidule",
+      apps: [meApp, teamApp, bagApp, scannerApp, pokedexApp, settingsApp],
     };
   }
 }
